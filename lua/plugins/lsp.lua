331fbd9ea3173edad617ff1916c97cdaa2396027
Pavol Porubsky <paulooze@gmail.com>
2023-10-30 00:17:26 +0100

    remove useless telescope actions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -64,19 +64,9 @@ return {
         "Type [D]efinition")
       nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols,
         "[D]ocument [S]ymbols")
-      nmap("<leader>ws",
-        require("telescope.builtin").lsp_dynamic_workspace_symbols,
-        "[W]orkspace [S]ymbols")
       nmap("K", vim.lsp.buf.hover, "Hover Documentation")
       nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
       nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-      nmap("<leader>wa", vim.lsp.buf.add_workspace_folder,
-        "[W]orkspace [A]dd Folder")
-      nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder,
-        "[W]orkspace [R]emove Folder")
-      nmap("<leader>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, "[W]orkspace [L]ist Folders")
 
       vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
         vim.lsp.buf.format()

ee6faef6dd5ffbee2b2fb94fb8193be09b555131
Pavol Porubsky <paulooze@gmail.com>
2023-11-19 01:34:53 +0100

    refactor; remove unused plugins

--- lua/plugins/cokeline.lua
+++ lua/plugins/cokeline.lua
@@ -1,23 +1,17 @@
 local icons = require('utils.icons')
 
 local function get_error_string(errors)
-  if errors > 0 then
-    return ' ' .. icons.diagnostics.Error .. ' ' .. errors .. ' '
-  else
-    return ''
-  end
+  return
+      errors > 0 and ' ' .. icons.diagnostics.Error .. ' ' .. errors .. ' ' or ''
 end
 local function get_warning_string(warnings)
-  if warnings > 0 then
-    return ' ' .. icons.diagnostics.Warning .. ' ' .. warnings .. ' '
-  else
-    return ''
-  end
+  return warnings > 0 and ' ' .. icons.diagnostics.Warning .. ' ' .. warnings ..
+      ' ' or ''
 end
 
 return {
   'willothy/nvim-cokeline',
-  dependencies = {'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons'},
+  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
   config = function()
     local is_picking_focus = require('cokeline.mappings').is_picking_focus
     local is_picking_close = require('cokeline.mappings').is_picking_close
@@ -28,60 +22,60 @@ return {
       default_hl = {
         fg = function(buffer)
           return buffer.is_focused and get_hex('ColorColumn', 'bg') or
-                   get_hex('Comment', 'fg')
+              get_hex('Comment', 'fg')
         end,
         bg = function(buffer)
           return buffer.is_focused and palette.blue or
-                   get_hex('ColorColumn', 'bg')
+              get_hex('ColorColumn', 'bg')
         end
       },
       components = {
-        {text = ' ', hl = {bg = get_hex('Normal', 'bg')}}, {
-          text = function(buffer)
-            return (is_picking_focus() or is_picking_close()) and
-                     buffer.pick_letter .. ' ' or buffer.devicon.icon
-          end,
-          fg = function(buffer)
-            return (is_picking_focus() and palette.orange) or
-                     (is_picking_close() and palette.red1) or
-                     buffer.devicon.color
-          end,
-          italic = function()
-            return (is_picking_focus() or is_picking_close())
-          end,
-          bold = function()
-            return (is_picking_focus() or is_picking_close())
+        { text = ' ', hl = { bg = get_hex('Normal', 'bg') } }, {
+        text = function(buffer)
+          return (is_picking_focus() or is_picking_close()) and
+              buffer.pick_letter .. ' ' or buffer.devicon.icon
+        end,
+        fg = function(buffer)
+          return (is_picking_focus() and palette.orange) or
+              (is_picking_close() and palette.red1) or
+              buffer.devicon.color
+        end,
+        italic = function()
+          return (is_picking_focus() or is_picking_close())
+        end,
+        bold = function()
+          return (is_picking_focus() or is_picking_close())
+        end
+      }, { text = ' ' }, {
+        fg = get_hex('Normal', 'bg'),
+        text = function(buffer)
+          if (buffer.is_modified) then
+            return 'M' .. ' '
+          else
+            return ''
           end
-        }, {text = ' '}, {
-          fg = get_hex('Normal', 'bg'),
-          text = function(buffer)
-            if (buffer.is_modified) then
-              return 'M' .. ' '
-            else
-              return ''
-            end
-          end,
-          bold = true
-        }, {
-          text = function(buffer) return buffer.filename end,
-          bold = function(buffer) return buffer.is_focused end
-        }, {text = '  '}, {
-          fg = palette.error,
-          text = function(buffer)
-            return get_error_string(buffer.diagnostics.errors)
-          end
-        }, {
-          fg = palette.warning,
-          text = function(buffer)
-            return get_warning_string(buffer.diagnostics.warnings)
-          end
-        }, {
-          text = ' ' .. icons.ui.Close,
-          on_click = function(_, _, _, _, buffer) buffer:delete() end
-        }, {text = ' ', hl = {bg = get_hex('Normal', 'bg')}}
+        end,
+        bold = true
+      }, {
+        text = function(buffer) return buffer.filename end,
+        bold = function(buffer) return buffer.is_focused end
+      }, { text = '  ' }, {
+        fg = palette.error,
+        text = function(buffer)
+          return get_error_string(buffer.diagnostics.errors)
+        end
+      }, {
+        fg = palette.warning,
+        text = function(buffer)
+          return get_warning_string(buffer.diagnostics.warnings)
+        end
+      }, {
+        text = ' ' .. icons.ui.Close,
+        on_click = function(_, _, _, _, buffer) buffer:delete() end
+      }, { text = ' ', hl = { bg = get_hex('Normal', 'bg') } }
       },
       sidebar = {
-        filetype = {'NvimTree'},
+        filetype = { 'NvimTree' },
         components = {
           {
             text = function(buf) return '  ' .. buf.filetype end,
--- lua/plugins/trouble.lua
+++
@@ -1,1 +0,0 @@
-return {'folke/trouble.nvim', event = 'VeryLazy'}
